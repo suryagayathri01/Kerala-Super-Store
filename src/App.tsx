377226502eca3cd4cdc30850c267078e9347bd07
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-01-24 15:16:05 +0530

    fixed layout of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <h1>eufgw</h1>
+      <Navbar />
+      <About />
+      <Gallery />
+      <Contact />
+      <Footer />
     </>
   );
 }
