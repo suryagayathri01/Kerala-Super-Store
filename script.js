9cf8333f16d6e8306ac0f3864b6d5a1f0ea87c63
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-01-24 14:59:28 +0530

    init

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-function ham() {
-    hamburgerMenu.classList.toggle("hidden");
-    console.log("clicked");
-}
