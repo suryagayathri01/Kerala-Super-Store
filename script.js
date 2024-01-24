0846c875fac426b62bdd407f5849370e0ac57060
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-01-24 13:53:11 +0530

    navbar layout

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+function ham() {
+    hamburgerMenu.classList.toggle("hidden");
+    console.log("clicked");
+}
