bae6b5477af67d5b352eb36e7d0fd38dd9598080
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-01-26 13:29:22 +0530

    added gradient design

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,13 @@
 const About = () => {
   return (
-    <div className="flex flex-col md:flex-row h-dvh items-center justify-center gap-10 sm:gap-44 bg-white text-center">
-      <div className="text-6xl font-bold ">About</div>
-      <div className="m-2 md:w-1/2 text-lg font-semibold">
+    <div className="flex h-dvh flex-col items-center justify-center gap-10 text-center sm:gap-44 md:flex-row">
+      <div className="flex flex-col gap-2 text-6xl font-bold text-transparent">
+        <div className="bg-gradient-to-r from-teal-300 to-indigo-400 bg-clip-text">
+          About
+        </div>
+        <div>rfhkb</div>
+      </div>
+      <div className="m-2 text-lg font-semibold md:w-1/2">
         Kerala Super Store, situated at the heart of Aloor, stands as a
         prominent supermarket with a rich history. Originating as a grocery
         store in the 1970s, the establishment underwent significant growth,
