5c782aa54ecece1bafffa6fa27e5024774109383
Suryagayathri K P <suryagayathrikp@gmail.com>
2024-01-28 20:24:21 +0530

    contact footer overlapping corrected

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,6 @@
 const Contact = () => {
   return (
-    <div className="h-screen  px-6 py-9 lg:px-5">
+    <div className="px-6 py-2 lg:px-5">
       <div className="mx-auto mb-6 mt-16 max-w-3xl rounded-md sm:mt-20">
         <div className="py-5 text-center text-3xl font-bold">Contact Us</div>
         <form
