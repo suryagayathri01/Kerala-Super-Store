36ff1fdfd93c2784445520730ebe2a0046b63ec6
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-01-30 16:24:11 +0530

    added carousel

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, A11y } from "swiper/modules";
+import { Navigation, Pagination, A11y, Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -8,6 +8,7 @@ import "swiper/css/scrollbar";
 
 const Gallery = () => {
   const images = [
+    { url: "./1.jpg", alt: "image 1" },
     { url: "./2.jpg", alt: "image 1" },
     { url: "./bg.webp", alt: "image 2" },
     { url: "./background.jpeg", alt: "image 3" },
@@ -15,17 +16,19 @@ const Gallery = () => {
 
   return (
     <>
-      <h1 className="p-4 text-center text-4xl">Gallery</h1>
+      <h1 className="p-4 text-center text-3xl font-bold">Gallery</h1>
       <Swiper
-        modules={[Navigation, Pagination, A11y]}
+        modules={[Navigation, Pagination, A11y, Autoplay]}
         spaceBetween={100}
         slidesPerView={1}
         navigation
+        autoplay={{ delay: 1000, disableOnInteraction: false }}
+        loop={true}
         pagination={{ clickable: true }}
-        className="w-full sm:w-1/2"
+        className="w-full sm:w-1/2 rounded-lg shadow-lg"
       >
         {images.map((image, index) => (
-          <SwiperSlide key={index} className="object-cover">
+          <SwiperSlide key={index} className="flex items-center justify-center">
             <img src={image.url} alt={image.alt} />
           </SwiperSlide>
         ))}
